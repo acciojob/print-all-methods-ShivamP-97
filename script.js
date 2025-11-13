73f896e53547f14bca7c7119fb046f8955029b5b
ShivamP-97 <shivampathak19972@gmail.com>
2025-11-13 14:21:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 //your JS code here. If required.
 function allMethods() {
-  //write your code here
+	const methods=Object.getOwnPropertyNames(Math)
+	.filter(prop=>typeof Math[prop]==="function");
+
+	return methods.join(", ");
 }
 
 alert(allMethods());
